069106d804dea321bb98abca0e3a9a4237f26522
Justyn <egot@istic.pw>
2020-12-26 02:57:52 -0600

    fix: properly wrap FileInfo

--- lib/dir.go
+++ lib/dir.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"context"
-	"log"
 	"mime"
 	"os"
 	"path"
@@ -10,19 +9,17 @@ import (
 	"golang.org/x/net/webdav"
 )
 
-type WebDavFileInfo struct {
+// NoSniffFileInfo wraps any generic FileInfo interface and bypasses mime type sniffing.
+type NoSniffFileInfo struct {
 	os.FileInfo
-	test bool
 }
 
-func (w *WebDavFileInfo) ContentType(ctx context.Context) (string, error) {
-	// TODO: remove debug logging
-	log.Println("NOT sniffing files")
-	if w.FileInfo.IsDir() {
-		return "inode/directory", nil
-	} else if mimeType := mime.TypeByExtension(path.Ext(w.FileInfo.Name())); mimeType != "" {
+func (w NoSniffFileInfo) ContentType(ctx context.Context) (contentType string, err error) {
+	if mimeType := mime.TypeByExtension(path.Ext(w.FileInfo.Name())); mimeType != "" {
+		// We can figure out the mime from the extension.
 		return mimeType, nil
 	} else {
+		// We can't figure out the mime type without sniffing, call it an octet stream.
 		return "application/octet-stream", nil
 	}
 }
@@ -33,22 +30,54 @@ type WebDavDir struct {
 }
 
 func (d WebDavDir) Stat(ctx context.Context, name string) (os.FileInfo, error) {
+	// Skip wrapping if NoSniff is off
 	if !d.NoSniff {
-		// TODO: remove debug logging
-		log.Println("USING THE DEFAULT STAT")
 		return d.Dir.Stat(ctx, name)
 	}
-	// TODO: remove debug logging
-	log.Println("USING THE WRAPPED STAT")
 
 	info, err := d.Dir.Stat(ctx, name)
-
 	if err != nil {
 		return nil, err
 	}
 
-	return WebDavFileInfo{
-		FileInfo: info,
-		test:     false,
-	}, nil
+	return NoSniffFileInfo{info}, nil
+}
+
+func (d WebDavDir) OpenFile(ctx context.Context, name string, flag int, perm os.FileMode) (webdav.File, error) {
+	// Skip wrapping if NoSniff is off
+	if !d.NoSniff {
+		return d.Dir.OpenFile(ctx, name, flag, perm)
+	}
+
+	file, err := d.Dir.OpenFile(ctx, name, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+
+	return WebDavFile{File: file}, nil
+}
+
+type WebDavFile struct {
+	webdav.File
+}
+
+func (f WebDavFile) Stat() (os.FileInfo, error) {
+	info, err := f.File.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	return NoSniffFileInfo{info}, nil
+}
+
+func (f WebDavFile) Readdir(count int) (fis []os.FileInfo, err error) {
+	fis, err = f.File.Readdir(count)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range fis {
+		fis[i] = NoSniffFileInfo{fis[i]}
+	}
+	return fis, nil
 }
